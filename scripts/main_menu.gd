45da06d40cfa3a346fc73dd42d88a90e998c7b7b
tgroess <tgroess@weltfern.com>
2025-11-07 20:46:53 +0100

    quit

---
+++ scripts/main_menu.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+
+func _on_start_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
