cf3c002845cc2cdbdeeb259f4875a10a924de150
tgroess <tgroess@weltfern.com>
2025-11-07 22:46:45 +0100

    menu fix

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -5,6 +5,7 @@ func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 func _on_back_to_menu_pressed() -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func _on_continue_pressed() -> void:
