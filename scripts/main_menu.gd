a241b47dc811a4e1e87af9e115c0b62febcc09c9
tgroess <tgroess@weltfern.com>
2025-11-08 10:09:25 +0100

    menu animation toggle

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+@onready var showCredits = false 
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/test_level.tscn")
@@ -10,4 +11,9 @@ func _on_quit_pressed() -> void:
 
 
 func _on_credits_pressed() -> void:
-	$AnimationPlayer.play("new_animation")
+	var animation = "show_credits" if !showCredits else "show_menu"
+	$AnimationPlayer.play(animation)
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	showCredits = anim_name == "show_credits"
