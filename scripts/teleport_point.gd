eee7abf13b0d6cfdd49615c16545e8fcb3b094b6
tgroess <tgroess@weltfern.com>
2025-11-08 14:17:56 +0100

    Farboption

--- scripts/teleport_point.gd
+++ scripts/teleport_point.gd
@@ -4,14 +4,23 @@ extends Marker3D
 @export var linked_teleporter : Marker3D
 
 @export_category("Visibility Variables")
-@export var show_debug_shape = false
+@export var show_debug_shape : bool = false
+@export var shape_color : Color = Color(Color.DARK_MAGENTA.r, Color.DARK_MAGENTA.g, Color.DARK_MAGENTA.b, 0.4)
 
 @onready var entered_from_teleport = false
 
 signal teleported_from(where)
 
+
 func _ready() -> void:
+	$Debug.get_active_material(0)
 	$Debug.visible = show_debug_shape
+	
+	var mesh = $Debug.mesh.duplicate()
+	var mat = mesh.material.duplicate()
+	mat.albedo_color = shape_color
+	mesh.material = mat
+	$Debug.mesh = mesh
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("PlayerCharacter") && !entered_from_teleport :
