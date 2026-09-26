dbdb73bb7917787d60bbd4047ed3b426f8446172
David Benson <DaveB-90@web.de>
2026-09-26 19:43:46 +0200

    grounded script updated

--- scripts/block.gd
+++ scripts/block.gd
@@ -5,6 +5,9 @@ var is_on_ground := false
 
 func _ready() -> void:
 	add_to_group("level_blocks")
+	contact_monitor = true
+	max_contacts_reported = 4
+	body_entered.connect(_on_body_entered)
 
 
 func _on_body_entered(body: Node) -> void:
